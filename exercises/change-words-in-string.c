f02ae0de128a7aa6197fda94d9ee826087eae1e9
Samuel <steixeira@ua.pt>
2023-08-21 22:01:20 +0100

    Exercise to replace words in string works, but it has memory leak problems

--- exercises/change-words-in-string.c
+++ exercises/change-words-in-string.c
@@ -5,16 +5,13 @@
 #define MAX_STRING_SIZE 200
 #define MAX_WORD_SIZE 20
 
-int replace_chars_in_string(char *string, char *word_to_replace, char *replacement_word);
+int replace_chars_in_string(char **string, char *word_to_replace, char *replacement_word);
 
 int main()
 {
     char *string = (char *)malloc(MAX_STRING_SIZE * sizeof(char));
     char *word_to_replace = (char *)malloc(MAX_WORD_SIZE * sizeof(char));
     char *replacement_word = (char *)malloc(MAX_WORD_SIZE * sizeof(char));
-
-    char *start_pointer = string;
-    char *end_pointer = string;
 
     printf("Enter your string: \n");
     fgets(string, MAX_STRING_SIZE, stdin);
@@ -28,7 +25,7 @@ int main()
     int boolean = 1;
     while (1)
     {
-        boolean = replace_chars_in_string(string, word_to_replace, replacement_word);
+        boolean = replace_chars_in_string(&string, word_to_replace, replacement_word);
         if (boolean == 0)
         {
             break;
@@ -42,9 +39,9 @@ int main()
     free(replacement_word);
 }
 
-int replace_chars_in_string(char *string, char *word_to_replace, char *replacement_word)
+int replace_chars_in_string(char **string, char *word_to_replace, char *replacement_word)
 {
-    char *occurence = strstr(string, word_to_replace);
+    char *occurence = strstr(*string, word_to_replace);
 
     if (occurence == NULL)
     {
@@ -53,32 +50,27 @@ int replace_chars_in_string(char *string, char *word_to_replace, char *replaceme
 
     int difference = strlen(replacement_word) - strlen(word_to_replace);
 
-    if (difference == 0)
-    {
-        // insert the replacement word
-        memcpy(occurence, replacement_word, strlen(replacement_word));
-    }
-
-    else if (difference > 0)
+    if (difference > 0)
     {
         // change size to size plus difference between the words
-        realloc(string, (MAX_STRING_SIZE + difference) * sizeof(char));
+        *string = realloc(*string, (MAX_STRING_SIZE + difference) * sizeof(char));
 
         // shift the chars after the word to replace to the furthest right
-        memcpy(occurence + strlen(replacement_word), occurence + strlen(word_to_replace), strlen(occurence + strlen(word_to_replace)) + 1);
-        // insert the replacement word
-        memcpy(occurence, replacement_word, strlen(replacement_word));
+        memmove(occurence + strlen(replacement_word), (occurence + strlen(word_to_replace)), strlen(occurence + strlen(word_to_replace)) + 1);
+        
     }
 
-    else
+    else if(difference < 0)
     {
         // shift chars after the word to replace to the furthest left
-        memcpy(occurence + strlen(replacement_word), occurence + strlen(word_to_replace), strlen(occurence + strlen(word_to_replace)) + 1);
+        memmove(occurence + strlen(replacement_word), occurence + strlen(word_to_replace), strlen(occurence + strlen(word_to_replace)) + 1);
         // difference is negative
-        realloc(string, (MAX_STRING_SIZE + difference) * sizeof(char));
-        // insert the replacement word
-        memcpy(occurence, replacement_word, strlen(replacement_word));
+        *string = realloc(*string, (MAX_STRING_SIZE + difference) * sizeof(char));
+        
     }
+    // insert the replacement word
+
+    memcpy(occurence, replacement_word, strlen(replacement_word));
 
     return 1;
 }
